afcf9c7c9f753b093f403b249b199c22beae9e88
Pradeep Kumar <143413556+yadav-124@users.noreply.github.com>
2025-03-29 16:03:15 +0530

    changed the project name

--- firstProject/contact/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from contact.models import Contact
-
-class contactAdmin(admin.ModelAdmin):
-    list_display=('name','email','phone','message')
-
-admin.site.register(Contact,contactAdmin)
-# Register your models here.
--- firstProject/firstProject/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class createForms(forms.Form):
-    num1=forms.IntegerField(label="Input-1",widget=forms.TextInput(attrs={'class':"form-control"}))
-    num2=forms.IntegerField(label="Input-2",widget=forms.TextInput(attrs={'class':"form-control"}))
